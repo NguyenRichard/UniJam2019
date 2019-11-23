d474c4783508426c636a6f2850bdee4748b499f2
Quentin Tran <quentin.tran@telecom-sudparis.eu>
2019-11-23 11:07:35 +0100

    Mise en place prefab Pilleur avec controle des animations

---
+++ JeuDeBoule/Assets/Scripts/PilleurAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilleurAnimator : MonoBehaviour
+{
+    Animator animator;
+
+    [SerializeField]
+    Transform holdingTransform;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Assert(animator, "Animtor can't be null");
+        Debug.Assert(holdingTransform, "Transform can't be null");
+
+        animator.SetBool("isWalking", true);
+    }
+
+    public void SetWalking(bool value)
+    {
+        animator.SetBool("isWalking", value);
+    }
+
+    public void Die()
+    {
+        animator.SetBool("isDead", true);
+    }
+
+    public void PickObject(GameObject obj)
+    {
+        obj.transform.parent = holdingTransform;
+    }
+}
