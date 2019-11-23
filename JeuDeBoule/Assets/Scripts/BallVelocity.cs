125fa534f3aa6747b28874db8726413d9483c127
NguyenRichard <36001056+NguyenRichard@users.noreply.github.com>
2019-11-23 12:32:54 +0100

    ControlBoard

---
+++ JeuDeBoule/Assets/Scripts/BallVelocity.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallVelocity : MonoBehaviour
+{
+
+    [SerializeField]
+    private float max_speed = 1;
+
+    [SerializeField]
+    private float dash_speed = 4;
+
+    private bool isDashing = false;
+    private Vector3 dash_direction;
+
+    private bool grounded = false;
+
+    Rigidbody rb;
+
+    float speedX;
+    float speedY;
+    float speedZ;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grounded)
+        {
+            if (!isDashing)
+            {
+                speedX = (Mathf.Abs(rb.velocity.x) > max_speed) ? max_speed * Mathf.Sign(rb.velocity.x) : rb.velocity.x;
+                speedZ = (Mathf.Abs(rb.velocity.z) > max_speed) ? max_speed * Mathf.Sign(rb.velocity.z) : rb.velocity.z;
+
+                rb.velocity = new Vector3(speedX, 0, speedZ);
+            }
+            else
+            {
+                rb.velocity = dash_direction * dash_speed;
+            }
+        }
+
+
+    }
+
+    public void StartDash(float x, float z)
+    {
+        if (!isDashing)
+        {
+            isDashing = true;
+            dash_direction = new Vector3(x, 0, z).normalized;
+            StartCoroutine("Dash");
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        yield return new WaitForSeconds(0.8f);
+        isDashing = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+    }
+}
---
+++ JeuDeBoule/Assets/Scripts/InputController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject testJoystick;
+
+    [SerializeField]
+    private Transform dungeon;
+
+    [SerializeField]
+    private GameObject ball;
+
+    private BallVelocity ballVelocity;
+
+    private Renderer renderer;
+
+    private float max_degree = 15;
+
+    private void Start()
+    {
+        renderer = testJoystick.GetComponent<Renderer>();
+        ballVelocity = ball.GetComponent<BallVelocity>();
+        Debug.Assert(ballVelocity, "You must add BallVelocity component to the ball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+             renderer.material.color = Color.green;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            renderer.material.color = Color.white;
+        }
+        if (Input.GetButtonDown("Dash"))
+        {
+            renderer.material.color = Color.red;
+
+        }
+        if (Input.GetButtonUp("Dash"))
+        {
+            renderer.material.color = Color.white;
+        }
+
+        UpdateDungeonPose();
+
+        Dash();
+
+
+    }
+
+    void UpdateDungeonPose()
+    {
+        float posX = Input.GetAxis("Horizontal");
+        float posY = Input.GetAxis("Vertical");
+
+        dungeon.eulerAngles = new Vector3(convertToDegree(posY), 0, -convertToDegree(posX));
+        //testJoystick.transform.position = new Vector2(posX, posY);
+
+    }
+
+    void Dash()
+    {
+        float posX = Input.GetAxisRaw("DHorizontal");
+        float posY = Input.GetAxisRaw("DVertical");
+        if(posX != 0 || posY != 0)
+        {
+            ballVelocity.StartDash(posX, posY);
+        }
+    }
+
+    /// <summary>
+    /// Convert GetAxis [-1;1] to [-max_degree, max_degree] in degrees.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    float convertToDegree(float value)
+    {
+        return value * max_degree;
+    }
+
+
+}
