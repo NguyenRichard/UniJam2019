4b3244716487eebcb631bc151a8c024089a6dd9f
Quentin Tran <quentin.tran@telecom-sudparis.eu>
2019-11-24 12:23:42 +0100

    AJout d'un menu tuto

---
+++ JeuDeBoule/Assets/Scripts/PopUp.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUp : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] tutosImg;
+    int id = 0;
+
+    [SerializeField]
+    Image image;
+
+    bool isOpen = false;
+    
+    void Start()
+    {
+        OpenPopUp();
+    }
+
+    public void OpenPopUp()
+    {
+        if( id < 3)
+        {
+            isOpen = true;
+            Time.timeScale = 0.0f;
+            image.sprite = tutosImg[id];
+        }
+        
+    }
+
+    public void ClosePopUp()
+    {
+       
+
+        if( id == 2)
+        {
+            isOpen = false;
+            Time.timeScale = 1.0f;
+            image.enabled = false;
+
+            //TODO : Lancer le jeu
+        }
+        id++;
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Confirm") && isOpen)
+        {
+            ClosePopUp();
+            OpenPopUp();
+        }   
+    }
+
+}
